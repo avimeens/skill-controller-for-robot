d24bea7a1f54961d0e479968c855a854b7a25da6
Avinash Vyas <avinash.vyas@nokia.com>
2018-07-11 16:16:53 -0400

    commit move command

--- __init__.py
+++ __init__.py
@@ -6,9 +6,22 @@ class ControllerForRobotSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder().require('ControllerForRobot'))
+    @intent_file_handler('robot.controller.intent'))
     def handle_controller_for_robot(self, message):
         self.speak_dialog('controller.for.robot')
+
+    @intent_file_handler('robot.move.intent')
+    def handle_ppt_open(self, message):
+    distance = message.data.get("distance")
+    if distance is None:
+        self.speak_dialog('robot.no.distance')
+        return
+    direction = message.data.get("direction")
+    if direction is None:
+        self.speak_dialgo('robot.no.direction')
+        return
+    resp = {'distance' : distance, 'direction' : direction}
+    self.speak_dialog('robot.move', data=resp)
 
 
 def create_skill():
